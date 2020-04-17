f43d418556c155e70b51fb87549b5d9ef8dc4171
Carla Holcomb <carla.programmer@gmail.com>
2020-04-17 16:11:24 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+  
+  before_validation :make_title_case
 
   private
 
@@ -14,4 +16,10 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+  
+   def make_title_case
+    # Rails provides a String#titlecase method
+    self.title = self.title.titlecase
+  end
+  
 end
