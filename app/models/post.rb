5656963515b739815531b553683f7d432994aab8
Carla Holcomb <carla.programmer@gmail.com>
2020-04-17 16:13:03 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
 
   belongs_to :author
   validate :is_title_case 
+  
+  before_validation :make_title_case
+  before_save :email_author_about_post
 
   private
 
@@ -14,4 +17,15 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+  
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
+  
+  def make_title_case
+    # Rails provides a String#titlecase method
+    self.title = self.title.titlecase
+  end
+  
 end
